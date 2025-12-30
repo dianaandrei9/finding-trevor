bb1b578639ae53453d490fa9f21f093f52bdd673
Diana Andrei <dianaandrei0907@gmail.com>
2025-12-30 14:22:10 +0200

    Fixed skel + basic window

---
+++ src/levels/level.py
@@ -0,0 +1,8 @@
+from src.settings import *
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+    def run(self):
+        self.display_surface.fill('gray')
