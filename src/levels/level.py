6555748f1430bb390c29aeaa7d35fc3f880de6fb
Diana Andrei <dianaandrei0907@gmail.com>
2025-12-31 01:10:38 +0200

    simple collision

--- src/levels/level.py
+++ src/levels/level.py
@@ -12,16 +12,17 @@ class Level:
 
         # groups
         self.all_sprites  = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
         
         self.setup(tmx_map)
         
     def setup(self, tmx_map):
         for x,y,surf in tmx_map.get_layer_by_name('platforms').tiles():
-            Sprite((x * TILE_SIZE , y * TILE_SIZE), surf, self.all_sprites)
+            Sprite((x * TILE_SIZE , y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites))
         
         for obj in tmx_map.get_layer_by_name('Player'):
             if obj.name == 'Tabitha':
-                Player((obj.x, obj.y), self.all_sprites)
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
             
     def update(self, dt: float):
         player = next((s for s in self.all_sprites if isinstance(s, Player)), None)
