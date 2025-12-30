4efd082c811ebc089327105461e4b363404ff13f
rottentshirt <catalinafasui@gmail.com>
2025-12-30 14:39:17 -0800

    Parallaxed that sht

---
+++ src/core/parallax_att.py
@@ -0,0 +1,42 @@
+import pygame
+import os
+
+class ParallaxLayer:
+    def __init__(self, image_path, speed):
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.speed = speed
+
+        self.x1 = 0
+        self.x2 = self.image.get_width()
+
+    def update(self, camera_dx):
+        self.x1 -= camera_dx * self.speed
+        self.x2 -= camera_dx * self.speed
+
+        w = self.image.get_width()
+
+        if self.x1 <= -w:
+            self.x1 = self.x2 + w
+        if self.x2 <= -w:
+            self.x2 = self.x1 + w
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x1, 0))
+        surface.blit(self.image, (self.x2, 0))
+
+
+class ParallaxBackground:
+    def __init__(self, folder_path, speeds):
+        self.layers = []
+
+        for i, speed in enumerate(speeds, start=1):
+            img_path = os.path.join(folder_path, f"{i}.png")
+            self.layers.append(ParallaxLayer(img_path, speed))
+
+    def update(self, camera_dx):
+        for layer in self.layers:
+            layer.update(camera_dx)
+
+    def draw(self, surface):
+        for layer in self.layers:
+            layer.draw(surface)
--- src/levels/level.py
+++ src/levels/level.py
@@ -1,5 +1,5 @@
 from src.settings import *
-from src.assets.parallax import ParallaxLayer
+from src.core.parallax_att import ParallaxBackground
 from src.assets.sprites import Sprite
 from src.entities.player import Player
 
@@ -7,6 +7,9 @@ class Level:
     def __init__(self, tmx_map, surface):
         self.display_surface = surface
         
+        from src.core.parallax_att import ParallaxBackground 
+        self.parallax = ParallaxBackground( folder_path="assets/parallax", speeds=[0.02, 0.05, 0.08, 0.12, 0.18, 0.25, 0.35, 0.5, 0.8] )
+
         # groups
         self.all_sprites  = pygame.sprite.Group()
         
@@ -21,9 +24,15 @@ class Level:
                 Player((obj.x, obj.y), self.all_sprites)
             
     def update(self, dt: float):
-        pass
+        player = next((s for s in self.all_sprites if isinstance(s, Player)), None)
+        if player:
+            camera_dx = player.direction.x * player.speed * dt 
+        else:
+            camera_dx = 0 
+        self.parallax.update(camera_dx)
     
     def run(self, dt):
         self.all_sprites.update(dt)
-        self.display_surface.fill('gray')
+        self.parallax.draw(self.display_surface)
+        self.all_sprites.update(dt)
         self.all_sprites.draw(self.display_surface)
