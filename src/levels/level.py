709884dc35155f2110c6744ec4cce07fbf994e9b
Diana Andrei <dianaandrei0907@gmail.com>
2025-12-31 13:43:15 +0200

    modif basic she can die and respawn

--- src/levels/level.py
+++ src/levels/level.py
@@ -2,6 +2,7 @@ from src.settings import *
 from src.core.parallax_att import ParallaxBackground
 from src.assets.sprites import Sprite
 from src.entities.player import Player
+from src.systems.collider import Collider
 
 class Level:
     def __init__(self, tmx_map, surface):
@@ -12,7 +13,10 @@ class Level:
         # groups
         self.all_sprites  = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
-        
+
+        # death zones        
+        self.death_rects = []
+
         self.setup(tmx_map)
         
     def setup(self, tmx_map):
@@ -21,15 +25,31 @@ class Level:
         
         for obj in tmx_map.get_layer_by_name('Player'):
             if obj.name == 'Tabitha':
-                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+        
+        # trigger
+        for obj in tmx_map.get_layer_by_name('block_movement'):
+                if obj.name == "death":
+                    self.death_rects.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+                if obj.name == "block_map":
+                    rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                    Collider(rect, self.collision_sprites)
             
-    def update(self, dt: float):
+    def update(self, dt: float):   
         player = next((s for s in self.all_sprites if isinstance(s, Player)), None)
         if player:
             camera_dx = player.actual_dx
         else:
             camera_dx = 0 
         self.parallax.update(camera_dx)
+        
+        # kill zone check
+        for r in self.death_rects:
+            if self.player.rect.colliderect(r):
+                # respawn
+                self.player.rect.topleft = (800, 244)
+                self.player.direction.y = 0
+                
     
     def run(self, dt):
         self.parallax.draw(self.display_surface)
---
+++ src/systems/collider.py
@@ -0,0 +1,7 @@
+from src.settings import *
+
+class Collider(pygame.sprite.Sprite):
+    def __init__(self, rect, groups):
+        super().__init__(groups)
+        self.rect = rect
+        self.old_rect = self.rect.copy()
