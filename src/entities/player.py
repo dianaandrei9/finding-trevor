dba0f7a85ed3d0463859dfdce6ea021cfb029d74
rottentshirt <catalinafasui@gmail.com>
2025-12-31 09:07:52 -0800

    fixed some jump stuff and added some draft animations

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,11 +1,12 @@
 from src.settings import *
 from src.systems.timer import Timer
 from src.systems.collision import Collision
+import os
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load("assets/sprites/tabitha.png").convert_alpha()
+        self.image = pygame.image.load("assets/sprites/Tabitha-fixed.png").convert_alpha()
 
         # rect
         self.rect = self.image.get_rect(topleft = pos)
@@ -29,6 +30,54 @@ class Player(pygame.sprite.Sprite):
             'wall jump': Timer(300),
             'jump wait': Timer(50)
         }
+        # animations
+        self.animations = {
+            'idle': [],
+            'walk': []
+        }
+
+        self.status = 'idle'
+        self.frame_index = 0
+        self.animation_speed = 10  # adjust if too fast/slow
+
+        # load frames
+        self.animations['idle'] = self.load_frames('assets/sprites/tabitha_standing_animation')
+        self.animations['walk'] = self.load_frames('assets/sprites/tabitha_running_animation')
+
+        # start with first idle frame
+        self.image = self.animations['idle'][0]
+
+
+    def load_frames(self, path):
+        frames = []
+        for filename in sorted(os.listdir(path)):
+            img = pygame.image.load(path + '/' + filename).convert_alpha()
+            frames.append(img)
+        return frames
+
+    def get_status(self):
+        if self.direction.x != 0:
+            self.status = 'walk'
+        else:
+            self.status = 'idle'
+
+    def animate(self, dt):
+        frames = self.animations[self.status]
+
+        # different speeds per animation
+        if self.status == 'idle':
+            speed = 2      # slow
+        elif self.status == 'walk':
+            speed = 10     # normal
+        else:
+            speed = 10
+
+        self.frame_index += speed * dt
+
+        if self.frame_index >= len(frames):
+            self.frame_index = 0
+
+        self.image = frames[int(self.frame_index)]
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -54,23 +103,11 @@ class Player(pygame.sprite.Sprite):
         # vertical
         old_y = self.rect.y
         # sliding on walls yeah
-        if not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['jump wait'].active:
-            self.direction.y = 0
-            self.rect.y += self.gravity / 10 * dt
+        if (self.direction.y > 0 and not self.on_surface['floor'] and any((self.on_surface['left'], self.on_surface['right']))):
+            self.direction.y = min(self.direction.y, self.gravity * 0.1)
         else:
-            self.direction.y += self.gravity / 2 * dt
+            self.direction.y += self.gravity * dt
             self.rect.y += self.direction.y * dt
-            self.direction.y += self.gravity / 2 * dt
-
-        if self.jump:
-            if self.on_surface['floor']:
-                self.direction.y = -self.jump_height
-                self.timers['jump wait'].activate()
-            elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['jump wait'].active:
-                self.timers['wall jump'].activate()
-                self.direction.y = -self.jump_height
-                self.direction.x = 1 if self.on_surface['left'] else -1
-            self.jump = False
 
         hit_vertical = self.collision_sprites.resolve(self.rect, self.old_rect, 'vertical')
         if hit_vertical:
@@ -88,3 +125,16 @@ class Player(pygame.sprite.Sprite):
         self.input()
         self.move(dt)
         self.on_surface = self.collision_sprites.check_contact(self.rect)
+        self.get_status()
+        self.animate(dt)
+
+        if self.jump:
+            if self.on_surface['floor']:
+                self.direction.y = -self.jump_height
+                self.timers['jump wait'].activate()
+            elif any((self.on_surface['left'], self.on_surface['right'])) and not self.timers['jump wait'].active:
+                self.timers['wall jump'].activate()
+                self.direction.y = -self.jump_height
+                self.direction.x = 1 if self.on_surface['left'] else -1
+            self.jump = False
+
